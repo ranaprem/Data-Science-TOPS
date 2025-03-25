7353958e99c0495599ec2bd062e4abf3186970ed
Rana Prem <premrana8145@gmail.com>
2025-03-25 18:18:30 +0530

    module_2_DS_python
    This is the module 2 of Data Science

---
+++ Assignment1/Python1/Practical/prem_DS_module_2/even odd num.py
@@ -0,0 +1,8 @@
+#problem no 1 -> to check number is even or odd 
+#below is my code and it's working perfect 
+num = int(input (" enter the number to check even and odd !! :- "))
+for i in range (1):
+    if num%2 == 0:
+        print(num,"is even number")
+    else:
+        print(num,"is odd number")
